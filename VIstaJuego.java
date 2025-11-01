17fceea5926c4efab3d1be33b37084e649e2763f
hnoriega-alt <hnoriega@unal.edu.co>
2025-11-01 10:48:09 -0500

    Add files via upload

---
+++ VIstaJuego.java
@@ -0,0 +1,42 @@
+// import javax.swing.*; // (Si usas Swing)
+// import java.awt.*;
+
+/**
+ * Representa la Interfaz Gráfica de Usuario (GUI)[cite: 48, 205].
+ * NO contiene lógica de juego, solo componentes visuales.
+ * (Esta clase DEBE estar en su propio archivo "VistaJuego.java")
+ */
+public class VistaJuego /* extends JFrame */ {
+    
+    // --- Componentes Visuales ---
+    // private JButton botonPedir;
+    // private JButton botonPlantarse;
+    // private JButton botonDoblar;
+    // private JButton botonDividir;
+    // private JLabel etiquetaSaldo;
+    // private JPanel panelCartasJugador;
+    // private JPanel panelCartasCrupier;
+    
+    public VistaJuego() {
+        // TODO: Configurar la ventana (JFrame)
+        // TODO: Inicializar y añadir todos los componentes visuales
+    }
+    
+    // --- Métodos para el Controlador ---
+    
+    // public void agregarListeners(ActionListener listener) {
+    //     botonPedir.addActionListener(listener);
+    //     botonPlantarse.addActionListener(listener);
+    //     // ... etc ...
+    // }
+    
+    // public void actualizarVista(EstadoJuego estado) {
+    //     // TODO: Actualizar etiquetas (saldo, puntuaciones)
+    //     // TODO: Dibujar las cartas en los paneles
+    //     // TODO: Habilitar/deshabilitar botones (ej. deshabilitar "Dividir") [cite: 302]
+    // }
+    
+    // public void mostrarMensaje(String mensaje) {
+    //     // TODO: Mostrar "¡Blackjack!", "Te has pasado", etc. [cite: 304]
+    // }
+}
